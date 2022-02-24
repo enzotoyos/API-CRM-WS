81cb1544745deea6dc14324c83e14107d7eddffe
enzotoyos <enzo.toyos@gmail.com>
2022-02-24 15:52:15 +0100

    ajout post admin

--- lib/routes/AdminRoute.ts
+++ lib/routes/AdminRoute.ts
@@ -1,7 +1,22 @@
 import express from "express";
 import { Router, Request, Response } from "express";
 import { FirebaseApp } from "firebase/app";
-import { addDoc, collection, getFirestore } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  query,
+  where,
+  getDocs,
+  doc,
+  getDoc,
+  setDoc,
+} from "firebase/firestore";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
 
 const AdminRoute = Router();
 const db = getFirestore();
@@ -14,9 +29,30 @@ const db = getFirestore();
  * @apiPermission Token
  *
  */
+
 AdminRoute.get("/", async (req: Request, res: Response) => {
-  let result = { success: true, message: "get info ok " };
-  res.status(200).send(result);
+  if (req.query.id) {
+    var id: string = req.query.id + "";
+    const docRef = doc(db, "admins", id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      res.status(200).send(docSnap.data());
+    } else {
+      let result = {
+        success: false,
+        message: "Aucun utilisateur ne correspond à cet ID",
+      };
+      res.status(403).send(result);
+    }
+  } else {
+    let result = {
+      success: false,
+      message: "Aucun ID renseigné",
+    };
+    res.status(403).send(result);
+  }
 });
 
 /**
@@ -28,21 +64,50 @@ AdminRoute.get("/", async (req: Request, res: Response) => {
  *
  */
 AdminRoute.post("/", async (req: Request, res: Response) => {
-  try {
-    const docRef = await addDoc(collection(db, "admins"), {
-      email: req.body.email,
-      phone: req.body.phone,
-      name: req.body.name,
-      surname: req.body.surname,
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-      organization: [],
-    });
-    console.log("docRef : " + docRef.id);
-    let result = { success: true, message: "Utilisateur Ajouté" };
-    res.status(200).send(result);
-  } catch (error: any) {
-    console.log(error);
+  const q = query(
+    collection(db, "admins"),
+    where("email", "==", req.body.email)
+  );
+
+  const auth = getAuth();
+  const querySnapshot = await getDocs(q);
+  var userID: string;
+
+  if (querySnapshot.empty == true) {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        req.body.email,
+        req.body.password
+      );
+      console.log(req.body);
+      const user = userCredential.user;
+      userID = user.uid + "";
+      console.log("user ID", userID);
+      const docRef = doc(db, "admins", userID);
+      setDoc(docRef, {
+        email: req.body.email,
+        phone: req.body.phone,
+        name: req.body.name,
+        surname: req.body.surname,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        organization: [],
+      });
+      let result = {
+        success: true,
+        message: "Utilisateur ajouté",
+        docRef: userID,
+      };
+      sendEmailVerification(auth.currentUser).then(() => {
+        res.status(200).send(result);
+      });
+    } catch (e) {
+      res.status(400).send({ "erreur lors de la création ": e });
+    }
+  } else {
+    let result = { success: false, message: "Utilisateur déjà existant" };
+    res.status(403).send(result);
   }
 });
 
