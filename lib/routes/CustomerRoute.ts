b2876b22b6e77af17e6ded127e6668d6e5f5940a
gpatruno <gaetan.patruno@ynov.com>
2022-02-25 16:03:42 +0100

    git add Custom algo

--- lib/routes/CustomerRoute.ts
+++ lib/routes/CustomerRoute.ts
@@ -1,15 +1,19 @@
 import express from "express";
 import { Router, Request, Response } from "express";
-import { DocumentData, getFirestore } from "firebase-admin/firestore";
+import { DocumentData, FieldValue, getFirestore } from "firebase-admin/firestore";
 import ICustomer from "../interface/ICustomer";
 import IResult from "../interface/IResult";
 import Interceptor from "../middleware/Interceptor";
 import MailController from "../controller/MailController";
+import TokenController from "../controller/TokenController";
 
 const CustomerRoute = Router();
 const db = getFirestore();
+const adminRef = db.collection('admins');
 const customerRef = db.collection('customers');
+const orgaRef = db.collection('organizations');
 const mailCtrl = new MailController();
+const tokenCtrl = new TokenController();
 
 CustomerRoute.post("/mail", Interceptor, async (req: Request, res: Response) => {
     mailCtrl.sendInitPwd('DEUPONT Jean', 'gaetan.patruno@ynov.com', 'monlink');
@@ -74,30 +78,47 @@ CustomerRoute.get("/:id", async (req: Request, res: Response) => {
  * @apiDescription Ajoute un client dans une organisation
  * @apiPermission Token
  *
+ * @apiBody {String} id             Mandatory of Organization
  * @apiBody {String} email          Mandatory Email of the User.
  * @apiBody {String} phone          Mandatory Lastname.
  * @apiBody {String} name           Mandatory First name of the User.
  * @apiBody {String} surname        Mandatory Lastname of the User.
  * @apiBody {String} filename       Optionnal base 64 of a file.
  * @apiBody {Number} Age            Optionnal age.
- * @apiBody {Array} Appointement    Mandatory Array of Appointement.
  */
-CustomerRoute.post("/", async (req: Request, res: Response) => {
+CustomerRoute.post("/", Interceptor, async (req: Request, res: Response) => {
     try {
-        const newCusto = await customerRef.add({
-            email: req.body.email,
-            phone: req.body.phone,
-            name: req.body.name,
-            surname: req.body.surname,
-            filename: '',
-            age: 0,
-            appointement: [],
-            createdAt: Date.now(),
-            updatedAt: Date.now(),
-            createdBy: '',
-        });
-        console.log("docRef : " + newCusto.id);
-        res.status(200).send({ success: true, message: "Client Ajouté" });
+        console.log(req.body);
+        const tokenDecod = tokenCtrl.getToken(req.headers.authorization);
+        const userDoc = adminRef.doc(tokenDecod.uid);
+
+        const doc = await userDoc.get();
+        if (!doc.exists) {
+            res.status(500).send({ success: false, message: "Votre compte Admin n'existe pas." });
+        } else {
+            const isOrga = doc.data().organization.includes(req.body.id);
+            if (isOrga) {
+                const newCusto = await customerRef.add({
+                    email: req.body.email,
+                    phone: req.body.phone,
+                    name: req.body.name,
+                    surname: req.body.surname,
+                    filename: '',
+                    age: 0,
+                    appointement: [],
+                    createdAt: Date.now(),
+                    updatedAt: Date.now(),
+                    createdBy: tokenDecod.uid,
+                });
+                const docOrga = orgaRef.doc(req.body.id);
+                await docOrga.update({
+                    customer: FieldValue.arrayUnion(newCusto.id)
+                });
+                res.status(200).send({ success: true, message: "Le client a été ajouté dans l'organisation", record: newCusto.id });
+            } else {
+                res.status(401).send({ success: false, message: "Votre n'avez pas accès a cette organisation." });
+            }
+        }
     } catch (error: any) {
         console.log(error);
         res.status(400).send({ success: false, message: 'Une erreur est survenue durant l\'ajout d\'un client.', error: error });
