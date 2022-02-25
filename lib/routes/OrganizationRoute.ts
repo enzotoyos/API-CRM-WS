15629602d2d9949b8056871c680549e274149627
gpatruno <gaetan.patruno@ynov.com>
2022-02-25 15:12:39 +0100

    Token + tokenDecode

--- lib/routes/OrganizationRoute.ts
+++ lib/routes/OrganizationRoute.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import { Router, Request, Response } from 'express';
-import { DocumentData, getFirestore, Timestamp } from "firebase-admin/firestore";
+import { DocumentData, FieldValue, getFirestore, Timestamp } from "firebase-admin/firestore";
 import IOrganization from "../interface/IOrganization";
 import IResult from "../interface/IResult";
 import Interceptor from "../middleware/Interceptor";
+import TokenController from "../controller/TokenController";
 
 const OrganizationRoute = Router();
 const db = getFirestore();
 const organizationRef = db.collection('organizations');
+const adminRef = db.collection('admins');
+const tokenCtrl = new TokenController();
 
 /**
  * @api {get} organization/ Get All Organization
@@ -17,7 +20,7 @@ const organizationRef = db.collection('organizations');
  * @apiPermission Token
  *
  */
- OrganizationRoute.get("/", Interceptor, async (req: Request, res: Response) => {
+OrganizationRoute.get("/", Interceptor, async (req: Request, res: Response) => {
     let result: IResult = { success: true, message: "La récupération des organisations a réussi.", record: [] };
 
     try {
@@ -41,7 +44,7 @@ const organizationRef = db.collection('organizations');
  * @apiPermission Token
  *
  */
- OrganizationRoute.get("/:id", async (req: Request, res: Response) => {
+OrganizationRoute.get("/:id", async (req: Request, res: Response) => {
     let result: IResult = { success: true, message: "La récupération de l\'organisation a réussi." };
 
     try {
@@ -71,15 +74,23 @@ const organizationRef = db.collection('organizations');
  * @apiBody {String} name           Mandatory  name of the Organization.
  * @apiBody {Array} customers        Mandatory Array of Customers.
  */
- OrganizationRoute.post("/", async (req: Request, res: Response) => {
-     console.log(req.body)
+OrganizationRoute.post("/", Interceptor, async (req: Request, res: Response) => {
+    console.log(req.body)
+    const tokenDecod = tokenCtrl.getToken(req.headers.authorization);
     try {
+        // On crée une organisation
         const newOrga = await organizationRef.add({
             address: req.body.address,
             name: req.body.name,
             customers: [],
             createdAt: Date.now(),
-            createdBy: '',
+            updatedAt: Date.now(),
+            createdBy: tokenDecod.uid
+        });
+        // Puis on l'ajoute dans le tableau de l'admin
+        const docAdmin = adminRef.doc(tokenDecod.uid);
+        const updatedAdmin = await docAdmin.update({
+            organization: FieldValue.arrayUnion(newOrga.id)
         });
         console.log("docRef : " + newOrga.id);
         res.status(200).send({ success: true, message: "Organisation Ajoutée" });
