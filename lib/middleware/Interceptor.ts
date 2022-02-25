a3dd2b5ce6ddfb8618b047fd3478b91b304f8dc2
gpatruno <gaetan.patruno@ynov.com>
2022-02-25 08:52:51 +0100

    add SDK admin

--- lib/middleware/Interceptor.ts
+++ lib/middleware/Interceptor.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
+import jwt from 'jsonwebtoken';
 
+const secureKey = (process.env.SECURE_KEY != undefined) ? process.env.SECURE_KEY : 'AZERTYUIOPKEY12345';
 
 function TokenInterceptor(request: express.Request, response: express.Response, next: express.NextFunction) {
     console.log(`${request.method}:${request.path} | Body : ${JSON.stringify(request.body)} Params : ${JSON.stringify(request.query)}  Token defined : ${request.headers.authorization !== undefined}`);
@@ -7,18 +9,22 @@ function TokenInterceptor(request: express.Request, response: express.Response, 
         const tokenHeader: string = request.headers.authorization;
         //Si authorization est indéfinit cela fait planter l'API pour rien
         if (tokenHeader) {
-            // const tokenDecode: any = jwt.verify(tokenHeader, secureKey);
+            const tokenDecode: any = jwt.verify(tokenHeader, secureKey);
+            console.log(tokenDecode);
 
-            // if (new Date(tokenDecode.expiresIn).toString() === 'Invalid Date') {
-            //     throw new Error("Date invalide : " + tokenDecode.expiresIn);
-            // }
-            // if (tokenDecode.expiresIn > new Date().toLocaleString('en-GB', { timeZone: 'Europe/Paris' })) {
-            //     //Token en cours de validité
-            //     next();
-            // } else {
-            //     //Si le Token n'est plus valide on Throw une erreur pour activer le catch
-            //     throw new Error("Votre jeton à expiré : " + tokenDecode.expiresIn);
-            // }
+            if (new Date(tokenDecode.expiresIn).toString() === 'Invalid Date') {
+                throw new Error("Date invalide : " + tokenDecode.expiresIn);
+            }
+
+            console.log();
+            
+            if (tokenDecode.expiresIn > new Date()) {
+                //Token en cours de validité
+                next();
+            } else {
+                //Si le Token n'est plus valide on Throw une erreur pour activer le catch
+                throw new Error("Votre jeton à expiré : " + tokenDecode.expiresIn);
+            }
         } else {
             throw new Error('Votre jeton n\'est pas définit. Vous devez vous connecter.');
         }
