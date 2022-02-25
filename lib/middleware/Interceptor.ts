c847bc915dab94a6b148fb9fbde3dd74e5064a74
gpatruno <gaetan.patruno@ynov.com>
2022-02-25 09:24:09 +0100

    get doc and collection

--- lib/middleware/Interceptor.ts
+++ lib/middleware/Interceptor.ts
@@ -8,26 +8,26 @@ function TokenInterceptor(request: express.Request, response: express.Response, 
     try {
         const tokenHeader: string = request.headers.authorization;
         //Si authorization est indéfinit cela fait planter l'API pour rien
-        if (tokenHeader) {
-            const tokenDecode: any = jwt.verify(tokenHeader, secureKey);
-            console.log(tokenDecode);
+        // if (tokenHeader) {
+        //     const tokenDecode: any = jwt.verify(tokenHeader, secureKey);
+        //     console.log(tokenDecode);
 
-            if (new Date(tokenDecode.expiresIn).toString() === 'Invalid Date') {
-                throw new Error("Date invalide : " + tokenDecode.expiresIn);
-            }
+        //     if (new Date(tokenDecode.expiresIn).toString() === 'Invalid Date') {
+        //         throw new Error("Date invalide : " + tokenDecode.expiresIn);
+        //     }
 
-            console.log();
+        //     console.log();
             
-            if (tokenDecode.expiresIn > new Date()) {
-                //Token en cours de validité
+        //     if (tokenDecode.expiresIn > new Date()) {
+        //         //Token en cours de validité
                 next();
-            } else {
-                //Si le Token n'est plus valide on Throw une erreur pour activer le catch
-                throw new Error("Votre jeton à expiré : " + tokenDecode.expiresIn);
-            }
-        } else {
-            throw new Error('Votre jeton n\'est pas définit. Vous devez vous connecter.');
-        }
+        //     } else {
+        //         //Si le Token n'est plus valide on Throw une erreur pour activer le catch
+        //         throw new Error("Votre jeton à expiré : " + tokenDecode.expiresIn);
+        //     }
+        // } else {
+        //     throw new Error('Votre jeton n\'est pas définit. Vous devez vous connecter.');
+        // }
     } catch (error: any) {
         console.error(error);
         response.status(401).json({
--- lib/routes/CustomerRoute.ts
+++ lib/routes/CustomerRoute.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { Router, Request, Response } from "express";
-import admin from "firebase-admin";
+import { DocumentData, getFirestore } from "firebase-admin/firestore";
 import ICustomer from "../interface/ICustomer";
 import IResult from "../interface/IResult";
 import Interceptor from "../middleware/Interceptor";
 
 const CustomerRoute = Router();
+const db = getFirestore();
+const customerRef = db.collection('customers');
 
 /**
  * @api {get} admin/ Get Admin
@@ -16,63 +18,64 @@ const CustomerRoute = Router();
  *
  */
 CustomerRoute.get("/", Interceptor, async (req: Request, res: Response) => {
-  console.log("GET OK", req.params);
-  let result: IResult = { success: true, message: "", record: [] };
+    let result: IResult = { success: true, message: "La récupération des clients a réussi.", record: [] };
 
-  //   try {
-  //     const querySnapshot = await getDocs(collection(db, "customers"));
-  //     querySnapshot.forEach((doc) => {
-  //       result.record.push(doc.data());
-  //     });
-  //   } catch (error: any) {
-  //     result.success = false;
-  //     console.log(error);
-  //   }
-
-  res.status(200).send(result);
+    try {
+        const snapshot = await customerRef.get();
+        snapshot.forEach(doc => {
+            result.record.push(doc.data());
+        });
+        res.status(200).send(result);
+    } catch (error: any) {
+        console.log(error);
+        res.status(400).send({ success: false, message: 'Une erreur est survenue durant la récupération d\'un client.', error: error });
+    }
 });
 
 /**
- * @api {get} admin/ Get Admin
+ * @api {get} admin/:id Get Customer by Id
  * @apiGroup Admin
- * @apiName getAdmin
- * @apiDescription Get Admin
+ * @apiName getCustomerById
+ * @apiDescription Get Customer
  * @apiPermission Token
  *
  */
 CustomerRoute.get("/:id", async (req: Request, res: Response) => {
-  console.log("get PARAM", req.params);
+    let result: IResult = { success: true, message: "La récupération du client a réussi." };
 
-  //   try {
-  //     const docRef = doc(db, "customers", req.params.id);
-  //     const docSnap: DocumentData = (await getDoc(docRef)).data();
-
-  //     console.log("docRef : ", docSnap);
-  //   } catch (error: any) {
-  //     console.log(error);
-  //   }
-
-  let result = { success: true, message: "Coucou" };
-  res.status(200).send(result);
+    try {
+        const custoRef = customerRef.doc(req.params.id);
+        const doc = await custoRef.get();
+        if (!doc.exists) {
+            console.log('No such document!');
+            result.message = 'Aucun client correspondant';
+        } else {
+            result.result = doc.data();
+        }
+        res.status(200).send(result);
+    } catch (error: any) {
+        console.log(error);
+        res.status(400).send({ success: false, message: 'Une erreur est survenue durant la récupération d\'un client.', error: error });
+    }
 });
 
 CustomerRoute.post("/", async (req: Request, res: Response) => {
-  //   try {
-  //     const docRef = await addDoc(collection(db, "admins"), {
-  //       email: req.body.email,
-  //       phone: req.body.phone,
-  //       name: req.body.name,
-  //       surname: req.body.surname,
-  //       createdAt: Date.now(),
-  //       updatedAt: Date.now(),
-  //       organization: [],
-  //     });
-  //     console.log("docRef : " + docRef.id);
-  //     let result = { success: true, message: "Utilisateur Ajouté" };
-  //     res.status(200).send(result);
-  //   } catch (error: any) {
-  //     console.log(error);
-  //   }
+    //   try {
+    //     const docRef = await addDoc(collection(db, "admins"), {
+    //       email: req.body.email,
+    //       phone: req.body.phone,
+    //       name: req.body.name,
+    //       surname: req.body.surname,
+    //       createdAt: Date.now(),
+    //       updatedAt: Date.now(),
+    //       organization: [],
+    //     });
+    //     console.log("docRef : " + docRef.id);
+    //     let result = { success: true, message: "Utilisateur Ajouté" };
+    //     res.status(200).send(result);
+    //   } catch (error: any) {
+    //     console.log(error);
+    //   }
 });
 
 export = CustomerRoute;
