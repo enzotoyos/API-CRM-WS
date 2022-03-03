214256c2b7802a91e42365726153392cce0517a8
enzotoyos <enzo.toyos@gmail.com>
2022-03-03 14:29:13 +0100

    test

--- lib/middleware/Interceptor.ts
+++ lib/middleware/Interceptor.ts
@@ -1,39 +1,62 @@
-import * as express from 'express';
-import jwt from 'jsonwebtoken';
+import * as express from "express";
+import jwt from "jsonwebtoken";
 import LoggerManager = require("../../config/Logger");
 const Logger = LoggerManager(__filename);
 
-const secureKey = (process.env.SECURE_KEY != undefined) ? process.env.SECURE_KEY : 'AZERTYUIOPKEY12345';
+const secureKey =
+  process.env.SECURE_KEY != undefined
+    ? process.env.SECURE_KEY
+    : "AZERTYUIOPKEY12345";
 
-function TokenInterceptor(request: express.Request, response: express.Response, next: express.NextFunction) {
-    try {
-        const tokenHeader: string = request.headers.authorization;
-        //Si authorization est indéfinit cela fait planter l'API pour rien
-        if (tokenHeader) {
-            const tokenDecode: any = jwt.verify(tokenHeader, secureKey);
-            Logger.info(`${request.method}:${request.baseUrl}${request.path} UID : ${tokenDecode.uid} | Body : ${JSON.stringify(request.body)} Params : ${JSON.stringify(request.query)}`);
-            console.log(`${request.method}:${request.baseUrl}${request.path} UID : ${tokenDecode.uid} | Body : ${JSON.stringify(request.body)} Params : ${JSON.stringify(request.query)}`);
-            if (new Date(tokenDecode.expiresIn).getTime() > new Date().getTime()) {
-                //Token en cours de validité
-                next();
-            } else {
-                //Si le Token n'est plus valide on Throw une erreur pour activer le catch
-                throw new Error("Votre jeton à expiré : " + tokenDecode.expiresIn);
-            }
-        } else {
-            throw new Error('Votre jeton n\'est pas définit. Vous devez vous connecter.');
-        }
-    } catch (error: any) {
-        Logger.error(error);
-        response.status(401).json({
-            success: false,
-            message: "Problème d'authentification.",
-            error: [{
-                code: "MIDDLEWARE",
-                title: error.message
-            }]
-        });
+function TokenInterceptor(
+  request: express.Request,
+  response: express.Response,
+  next: express.NextFunction
+) {
+  try {
+    const tokenHeader: string = request.headers.authorization;
+    //Si authorization est indéfinit cela fait planter l'API pour rien
+    if (tokenHeader) {
+      const tokenDecode: any = jwt.verify(tokenHeader, secureKey);
+      Logger.info(
+        `${request.method}:${request.baseUrl}${request.path} UID : ${
+          tokenDecode.uid
+        } | Body : ${JSON.stringify(request.body)} Params : ${JSON.stringify(
+          request.query
+        )}`
+      );
+      console.log(
+        `${request.method}:${request.baseUrl}${request.path} UID : ${
+          tokenDecode.uid
+        } | Body : ${JSON.stringify(request.body)} Params : ${JSON.stringify(
+          request.query
+        )}`
+      );
+      if (new Date(tokenDecode.expiresIn).getTime() > new Date().getTime()) {
+        //Token en cours de validité
+        next();
+      } else {
+        //Si le Token n'est plus valide on Throw une erreur pour activer le catch
+        throw new Error("Votre jeton à expiré : " + tokenDecode.expiresIn);
+      }
+    } else {
+      throw new Error(
+        "Votre jeton n'est pas définit. Vous devez vous connecter."
+      );
     }
+  } catch (error: unknown) {
+    Logger.error(error);
+    response.status(401).json({
+      success: false,
+      message: "Problème d'authentification.",
+      error: [
+        {
+          code: "MIDDLEWARE",
+          title: error,
+        },
+      ],
+    });
+  }
 }
 
 export = TokenInterceptor;
