c18c5c3991b50fa091a6aaa041000d9594c72d2a
gpatruno <gaetan.patruno@ynov.com>
2022-03-04 10:16:45 +0100

    Algo Verif

--- lib/controller/adminController.ts
+++ lib/controller/adminController.ts
@@ -1,171 +1,191 @@
 import { IncomingMessage } from "http";
 import https from "https";
 import {
-  DocumentData,
-  FieldValue,
-  getFirestore,
+    getFirestore,
 } from "firebase-admin/firestore";
 import IResult from "../interface/IResult";
-import { URL } from "url";
+import LoggerManager from "../../config/Logger";
 
 const db = getFirestore();
+const Logger = LoggerManager(__filename);
 const adminRef = db.collection("admins");
 const orgaRef = db.collection("organizations");
 const custoRef = db.collection("customers");
 
 class AdminController {
-  /**
-   * checkAutorisationOrgaForAdmin
-   *
-   * @param idAdmin uid from token
-   * @param idOrganization id Orga
-   * @returns Promise<boolean>
-   */
-  async checkAutorisationOrgaForAdmin(
-    idAdmin: string,
-    idOrganization: string
-  ): Promise<boolean> {
-    let result: boolean = false;
-    const docUser = adminRef.doc(idAdmin);
-    const doc = await docUser.get();
 
-    if (!doc.exists) {
-      return result;
-    } else {
-      const isOrga = doc.data().organization.includes(idOrganization);
-      return isOrga;
-    }
-  }
+    /**
+     * checkAutorisationOrgaForAdmin
+     *
+     * @param idAdmin uid from token
+     * @param idOrganization id Orga
+     * @returns Promise<boolean>
+     */
+    async checkAutorisationOrgaForAdmin(
+        idAdmin: string,
+        idOrganization: string
+    ): Promise<boolean> {
+        let result: boolean = false;
+        const docUser = adminRef.doc(idAdmin);
+        const doc = await docUser.get();
 
-  /**
-   * checkAutorisationCustForAdmin
-   *
-   * UIDadmin -> Récupération des organisations
-   *
-   * @param idAdmin
-   * @param idOrganization
-   * @returns
-   */
-  async checkAutorisationCustForAdmin(
-    idAdmin: string,
-    idCustomer: string
-  ): Promise<boolean> {
-    let result = false;
-
-    const docUser = adminRef.doc(idAdmin);
-    const doc = await docUser.get();
-    const listIdOrga: String[] = doc.data().organization;
-
-    for (let index in listIdOrga) {
-      const docOrga = orgaRef.doc(String(listIdOrga[index]));
-      const doc = await docOrga.get();
-      if (doc.data().customer && doc.data().customer.length > 0) {
-        result = doc.data().customer.includes(idCustomer);
-      }
-
-      if (result) {
-        break;
-      }
+        if (!doc.exists) {
+            return result;
+        } else {
+            const isOrga = doc.data().organization.includes(idOrganization);
+            return isOrga;
+        }
     }
 
-    return result;
-  }
+    /**
+     * checkAutorisationCustForAdmin
 
-  /**
-   * checkAutorisationRdvForAdmin
-   *
-   * UIDadmin -> Récupération des organisations
-   *
-   * @param idAdmin
-   * @param idRdv
-   * @returns
-   */
-  async checkAutorisationRdvForAdmin(
-    idAdmin: string,
-    idRdv: string
-  ): Promise<boolean> {
-    let result = false;
+     * @param idAdmin
+     * @param idOrganization
+     * @returns
+     */
+    async checkAutorisationCustForAdmin(
+        idAdmin: string,
+        idCustomer: string
+    ): Promise<boolean> {
+        let result = false;
 
-    return result;
-  }
+        const docUser = adminRef.doc(idAdmin);
+        const doc = await docUser.get();
+        const listIdOrga: String[] = doc.data().organization;
 
-  /**
-   * login
-   *
-   * https://firebase.google.com/docs/reference/rest/auth/#section-sign-in-email-password
-   *
-   * @param email
-   * @param password
-   * @return object
-   */
-  async login(email: string, password: string): Promise<IResult> {
-    let result: IResult = { success: false, record: {} };
-    const API_KEY = process.env.API_KEY;
-    const body = JSON.stringify({
-      password: password,
-      email: email,
-      returnSecureToken: true,
-    });
-    const options = {
-      hostname: "identitytoolkit.googleapis.com",
-      port: 443,
-      path: "/v1/accounts:signInWithPassword?key=" + API_KEY,
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Content-Length": body.length,
-      },
-    };
+        for (let index in listIdOrga) {
+            const docOrga = orgaRef.doc(String(listIdOrga[index]));
+            const doc = await docOrga.get();
+            if (doc.data().customer && doc.data().customer.length > 0) {
+                result = doc.data().customer.includes(idCustomer);
+            }
 
-    try {
-      const record = await this.wrapedSendRequest(options, body);
-      if (record.statusCode && record.statusCode === 200) {
-        result.success = true;
-        result.record = record;
+            if (result) {
+                break;
+            }
+        }
+
         return result;
-      } else {
-        result.success = false;
-        result.error = record;
-        return result;
-      }
-    } catch (error) {
-      console.log(error);
-      result.success = false;
-      result.error = error;
-      return result;
     }
-  }
 
-  /**
-   * wrapedSendRequest
-   * Permet de rendre la fonction https request async
-   *
-   * @param options
-   * @param body
-   * @returns Promise<any>
-   */
-  async wrapedSendRequest(options, body): Promise<any> {
-    return new Promise((resolve, reject) => {
-      const req = https
-        .request(options, (res: IncomingMessage) => {
-          console.log(`statusCode: ${res.statusCode}`);
-          res.setEncoding("utf8");
-          let data = "";
-          res.on("data", (d) => {
-            data += d;
-          });
-          res.on("end", () => {
-            let result = JSON.parse(data);
-            result["statusCode"] = res.statusCode;
-            resolve(result);
-          });
-        })
-        .on("error", (err) => {
-          reject(err);
-        })
-        .write(body);
-    });
-  }
+    /**
+     * checkAutorisationRdvForAdmin
+
+     * @param idAdmin string
+     * @param idRdv string
+     * @returns Promise<boolean>
+     */
+    async checkAutorisationRdvForAdmin(idAdmin: string, idRdv: string): Promise<boolean> {
+        let result = false;
+
+        const docUser = adminRef.doc(idAdmin);
+        const doc = await docUser.get();
+        const listIdOrga: String[] = doc.data().organization;
+
+        for (let index in listIdOrga) {
+            const docOrga = orgaRef.doc(String(listIdOrga[index]));
+            const doc = await docOrga.get();
+            const listIdCusto: String[] = doc.data().customer;
+            for (let iterator in listIdCusto) {
+                const docCusto = custoRef.doc(String(listIdCusto[iterator]));
+                const docData = await docCusto.get();
+
+                if (docData.data().appointement && docData.data().appointement.length > 0) {
+                    result = docData.data().appointement.includes(idRdv);
+                }
+
+                if (result) {
+                    break;
+                }
+            }
+
+            if (result) {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * login
+     *
+     * https://firebase.google.com/docs/reference/rest/auth/#section-sign-in-email-password
+     *
+     * @param email
+     * @param password
+     * @return object
+     */
+    async login(email: string, password: string): Promise<IResult> {
+        let result: IResult = { success: false, record: {} };
+        const API_KEY = process.env.API_KEY;
+        const body = JSON.stringify({
+            password: password,
+            email: email,
+            returnSecureToken: true,
+        });
+        const options = {
+            hostname: "identitytoolkit.googleapis.com",
+            port: 443,
+            path: "/v1/accounts:signInWithPassword?key=" + API_KEY,
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Content-Length": body.length,
+            },
+        };
+
+        try {
+            const record = await this.wrapedSendRequest(options, body);
+            if (record.statusCode && record.statusCode === 200) {
+                result.success = true;
+                result.record = record;
+                return result;
+            } else {
+                result.success = false;
+                result.error = record;
+                return result;
+            }
+        } catch (error) {
+            Logger.log({ level: "error", message: error });
+            result.success = false;
+            result.error = error;
+            return result;
+        }
+    }
+
+    /**
+     * wrapedSendRequest
+     * Permet de rendre la fonction https request async
+     *
+     * @param options
+     * @param body
+     * @returns Promise<any>
+     */
+    async wrapedSendRequest(options, body): Promise<any> {
+        return new Promise((resolve, reject) => {
+            const req = https
+                .request(options, (res: IncomingMessage) => {
+                    console.log(`statusCode: ${res.statusCode}`);
+                    res.setEncoding("utf8");
+                    let data = "";
+                    res.on("data", (d) => {
+                        data += d;
+                    });
+                    res.on("end", () => {
+                        let result = JSON.parse(data);
+                        result["statusCode"] = res.statusCode;
+                        resolve(result);
+                    });
+                })
+                .on("error", (err: any) => {
+                    Logger.log({ level: "error", message: err });
+                    reject(err);
+                })
+                .write(body);
+        });
+    }
 }
 
 export = AdminController;
